10a881a1614d8272ac6144867d61454084e7b714
Jhyke <igorduifhuizen@gmail.com>
2025-03-26 22:03:55 +0100

    Setup for inventory system

---
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+
+    public static InventoryManager Instance;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
