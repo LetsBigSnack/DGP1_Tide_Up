93e7107a8a4bf89a4b1e0ec9fc12ef148d5721f0
dekisenpaitm <dekisenpaitm@gmail.com>
2025-04-10 22:02:14 +0200

    Added First Scripts and the scrollview

---
+++ Assets/UIInventoryController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Assets.Scripts.Data;
+
+public class UIInventoryController : UIJournalSubMenu
+{
+
+    public override void CloseMenu()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OpenMenu()
+    {
+        throw new System.NotImplementedException();
+    }
+}
