4a31fb4fa9a9ed878441ca489e6e0e9d80cfe61e
dekisenpaitm <dekisenpaitm@gmail.com>
2025-04-11 22:40:30 +0200

    Implemented a way to sort the inventory based on the dropdown

--- Assets/UIInventoryHelper.cs
+++ Assets/UIInventoryHelper.cs
@@ -2,10 +2,16 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Data;
+using System.Linq;
 
-public enum FilterType
+public enum SortingType
 {
-
+    All,
+    Glass,
+    Metal,
+    Paper,
+    Plastic,
+    Wood
 }
 public class UIInventoryHelper : MonoBehaviour
 {
@@ -13,6 +19,7 @@ public class UIInventoryHelper : MonoBehaviour
 
     [Header("Filter")]
     [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private SortingType currentSortingType;
 
     [Header("ScrollViewContent")]
     [SerializeField] private Transform itemParent;
@@ -25,6 +32,31 @@ public class UIInventoryHelper : MonoBehaviour
 
     private List<GameObject> _currentItems = new List<GameObject>();
 
+    private Dictionary<int, SortingType> _dropdownOptions = new Dictionary<int, SortingType>()
+    {
+        { 0,SortingType.All },
+        { 1,SortingType.Glass },
+        { 2,SortingType.Metal },
+        { 3,SortingType.Paper },
+        { 4,SortingType.Plastic },
+        { 5,SortingType.Wood }
+    };
+
+    private Dictionary<SortingType, TrashMaterialType> __materialTypes = new Dictionary<SortingType, TrashMaterialType>()
+    {
+        {SortingType.Glass, TrashMaterialType.Glass},
+        {SortingType.Metal, TrashMaterialType.Metal},
+        {SortingType.Paper, TrashMaterialType.Paper},
+        {SortingType.Plastic, TrashMaterialType.Plastic},
+        {SortingType.Wood, TrashMaterialType.Wood},
+
+    };
+
+    private void Awake()
+    {
+        FillSortingOptions();
+    }
+
     private void OnEnable()
     {
         UpdateInventory(InventoryManager.Instance.Items);
@@ -36,6 +68,12 @@ public class UIInventoryHelper : MonoBehaviour
         InventoryManager.OnInventoryChanged -= UpdateInventory;
     }
 
+    public void SelectSortingType()
+    {
+        currentSortingType = _dropdownOptions[dropdown.value];
+        UpdateInventory(InventoryManager.Instance.Items);
+    }
+
     private void UpdateInventory(List<ItemInstance> items)
     {
         if(items == null)
@@ -44,12 +82,27 @@ public class UIInventoryHelper : MonoBehaviour
         }
 
         ClearInventory();
+        items = SortInventory(items);
         foreach(ItemInstance item in items)
         {
             GameObject newItem = Instantiate(itemPrefab, itemParent);
             newItem.GetComponent<UIInventoryItem>().Setup(item);
             _currentItems.Add(newItem);
         }
+    }
+    private List<ItemInstance> SortInventory(List<ItemInstance> items)
+    {
+        List<ItemInstance> sortedItems;
+
+        if (currentSortingType == SortingType.All)
+        {
+            return sortedItems = items;
+        }
+
+        return sortedItems = items
+            .OrderByDescending(item => item.GetMaterials()
+            .Any(m => m.type == __materialTypes[currentSortingType]))
+            .ToList();
     }
 
     private void ClearInventory()
@@ -66,4 +119,13 @@ public class UIInventoryHelper : MonoBehaviour
         _currentItems.Clear();
     }
 
+    private void FillSortingOptions()
+    {
+        List<string> dropdownOptions = new List<string>();
+        foreach(int i in _dropdownOptions.Keys)
+        {
+            dropdownOptions.Add(_dropdownOptions[i].ToString());
+        }
+        dropdown.AddOptions(dropdownOptions);
+    }
 }
