18fd7c8eada4c9fc0af2641458b69481f24e3f87
LetsBigSnack <dkupert.office@gmail.com>
2025-04-22 09:09:02 +0200

    Update UIStartSceneManager.cs

--- Assets/Scripts/Managers/UI/UIStartSceneManager.cs
+++ Assets/Scripts/Managers/UI/UIStartSceneManager.cs
@@ -6,6 +6,13 @@ public class UIStartSceneManager : MonoBehaviour
     [SerializeField] private Scenes sceneToLoad;
     public void StartGame()
     {
+        var player = Player.Instance;
+        if (player != null)
+        {
+            Debug.Log("Before move: " + player.transform.position);
+            player.transform.position += Vector3.up * 105f;
+            Debug.Log("After move: " + player.transform.position);
+        }
         GameStateManager.Instance?.ResumeGame();
         UIPauseMenuManager.Instance?.ResumeGame();
         StartCoroutine(SceneChangeManager.Instance.LoadSceneWithState(sceneToLoad));
