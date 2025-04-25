46ea60786b4c33514679d48e679bc4976f1fc10c
Jhyke <igorduifhuizen@gmail.com>
2025-04-25 18:15:40 +0200

    Fix for time skip

--- Assets/Scripts/Managers/MenuController.cs
+++ Assets/Scripts/Managers/MenuController.cs
@@ -60,7 +60,7 @@ public class MenuController : MonoBehaviour
         UIJournalManager.Instance.CloseAllMenues();
         UIReUpcycleManager.Instance.CloseAllMenues();
         UIShopManager.Instance.CloseAllMenues();
-        UITimeManager.Instance.CloseTimeChange();
+        UITimeManager.Instance.CancelTimeChange();
     }
     
     private void HandleEscape(InputAction.CallbackContext value)
