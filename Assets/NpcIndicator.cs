baa35bc8998a85dd1f0b55955fbfb7cd8454cb37
dekisenpaitm <dekisenpaitm@gmail.com>
2025-05-12 17:07:46 +0200

    Quest Indicators done

--- Assets/NpcIndicator.cs
+++ Assets/NpcIndicator.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class NpcIndicator : MonoBehaviour
 {
+    [SerializeField] private GameObject questionIndicator;
+    [SerializeField] private GameObject exclamationIndicator;
+    [SerializeField] private GameObject readyToDeliverIndicator;
     [SerializeField] private GameObject awarenessPrefab;
     [SerializeField] private Transform worldCanvas;
 
@@ -28,15 +31,65 @@ public class NpcIndicator : MonoBehaviour
     {
         NpcInteractable npcIndicator = npc.gameObject.GetComponent<NpcInteractable>();
 
-        if (npcIndicator == null || !npcIndicator.isActiveAndEnabled || GameStateManager.Instance.GetGameState() == GameStates.Dialogue || npc.NpcData.NpcState == NpcStates.Finished)
+        if (npcIndicator == null || !npcIndicator.isActiveAndEnabled || GameStateManager.Instance.GetGameState() == GameStates.Dialogue)
         {
+            questionIndicator.SetActive(false);
+            exclamationIndicator.SetActive(false);
+            readyToDeliverIndicator.SetActive(false);
             return;
         }
+
+        if (npc.NpcState == NpcStates.Intro)
+        {
+            exclamationIndicator.SetActive(true);
+            return;
+        }
+
+        if (npc.CurrentQuest?.QuestState == QuestState.Offer &&
+            npc.CompletedQuests < npc.MaxCompletedQuests &&
+            GameStateManager.Instance.GetGameState() != GameStates.Dialogue)
+        {
+            exclamationIndicator.SetActive(true);
+            questionIndicator.SetActive(false);
+            readyToDeliverIndicator.SetActive(false);
+            return;
+        }
+
+        if (npc.CurrentQuest?.QuestState == QuestState.InProgress &&
+            npc.CompletedQuests < npc.MaxCompletedQuests &&
+            GameStateManager.Instance.GetGameState() != GameStates.Dialogue)
+        {
+            if (InventoryManager.Instance.HasItem(npc.CurrentQuest.QuestItem))
+            {
+                readyToDeliverIndicator.SetActive(true);
+                questionIndicator.SetActive(false);
+                exclamationIndicator.SetActive(false);
+            }
+            else
+            {
+                questionIndicator.SetActive(true);
+                readyToDeliverIndicator.SetActive(false);
+                exclamationIndicator.SetActive(false);
+            }
+            return;
+        }
+
+        if (npc.NpcState == NpcStates.Finished)
+        {
+            questionIndicator.SetActive(false);
+            exclamationIndicator.SetActive(false);
+            readyToDeliverIndicator.SetActive(false);
+            return;
+        }
+
+        questionIndicator.SetActive(false);
+        exclamationIndicator.SetActive(false);
+        readyToDeliverIndicator.SetActive(false);
     }
 
     private void InstantiateAwarenessSlider(int completedQuests, Npc npc)
     {
-        if(this.npc != npc)
+        if (this.npc != npc)
         {
             return;
         }
