20b2098c46dd3f51f79f654da57d08c088785ca0
LetsBigSnack <dkupert.office@gmail.com>
2025-05-20 12:09:50 +0200

    Fix for Material Shit

--- Assets/FishingSpot.cs
+++ Assets/FishingSpot.cs
@@ -51,15 +51,5 @@ public class FishingSpot : Interactable
     {
         highlight = show;
     }
-
-    private void OnDrawGizmos()
-    {
-        if (highlight)
-        {
-            BoxCollider collider = GetComponentInChildren<BoxCollider>();
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(transform.position, collider.size);
-        }
-        
-    }
+    
 }
