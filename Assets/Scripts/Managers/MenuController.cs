243c0f788576291f133993b8bd7f8f7ad9795cf0
dekisenpaitm <dekisenpaitm@gmail.com>
2025-06-08 14:27:31 +0200

    add building

--- Assets/Scripts/Managers/MenuController.cs
+++ Assets/Scripts/Managers/MenuController.cs
@@ -232,7 +232,7 @@ public class MenuController : MonoBehaviour
         GameStateManager.Instance.SetGameState(GameStateManager.Instance.LastPlayingState);
         //TODO: Grenus Fix
         //UIHUDManager.Instance.ToggleDateMap();
-        //UIBuildManager.Instance.CloseMenu();
+        UIBuildManager.Instance.CloseMenu();
         UIJournalManager.Instance.SwitchState(JournalType.Closed);
         UIReUpcycleManager.Instance.SwitchState(ReUpcyclerType.Closed);
         UIShopManager.Instance.SwitchState(ShopType.Closed);
